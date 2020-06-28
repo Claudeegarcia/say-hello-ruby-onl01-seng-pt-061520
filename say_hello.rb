b521def627a01ad3e297dcaa774268ec47874316
Claudia Garcia <claudeegarcia@icloud.com>
2020-06-28 04:40:07 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,6 +1,13 @@
 # I call on the method, say_hello, and give it the string "Gabriela" 
+<<<<<<< HEAD
 def say_hello("Gabriela")
  
 # The method prints this text to the screen:
 Hello Gabriela!
 end
+=======
+say_hello("Gabriela")
+ 
+# The method prints this text to the screen:
+Hello Gabriela!
+>>>>>>> 260c7218f8fff84a28fa2091957d511c814b5864
